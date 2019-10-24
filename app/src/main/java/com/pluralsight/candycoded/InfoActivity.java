106b4511d4fc6b6bd333e5b1064eeb76df0ad7d3
MoudenFreeLance <moudenanalytics@gmail.com>
2019-10-24 10:31:51 +0100

    jv

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -28,7 +28,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
-
+    public void createMapIntent(View view){
+        Uri uriAdress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAdress);
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
